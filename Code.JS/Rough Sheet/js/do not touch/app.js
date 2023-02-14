d569d9767793ccf6dcb2a01183167d0412ec65c0
Ronaldo Excellent <120473458+ronaldoexcellent@users.noreply.github.com>
2023-02-14 18:56:59 +0100

    Delete Code.JS/Rough Sheet directory

--- Code.JS/Rough Sheet/js/do not touch/app.js
+++
@@ -1,1 +0,0 @@
-var application = document.querySelector('application'); var app = {name : application.getAttribute('name'), logo : "<img id='sitelogo' src='" + application.getAttribute('logo') + "' />", author : document.querySelector('meta-name').getAttribute('author'), copyright : document.querySelector('meta-name').getAttribute('copyright')};
--- Code.JS/Rough Sheet/js/do not touch/scripts.js
+++
@@ -1,5 +0,0 @@
-$import = ["do not touch/app.js", "do not touch/layout.js", "do not touch/build.js", "do not touch/functions.js", "do not touch/extras.js", "do not touch/terminal-config.js", "constructor.js", "script.js"];
-
-for ($imp = 0; $imp < $import.length; $imp++) {
-    $new_script = document.createElement('script'); $new_script.src = "js/" + $import[$imp]; document.querySelector('engine').appendChild($new_script);
-}
